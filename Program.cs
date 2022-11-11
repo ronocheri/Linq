da431f0eb0a3167c0e72b3e4760f4b1af464e924
ronocheri <ronocheri@gmail.com>
2022-11-11 11:04:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//print all persons' names contain 'a' - Query + Method syntax
+List<string> persons = new List<string>();
+persons.Add("Ron");
+persons.Add("Sean");
+persons.Add("Mor");
+persons.Add("Barak");
+persons.Add("Maor");
+
+// Query Syntax
+var res =from person in persons
+        where person.Contains('a')
+        select person;
+
+// Method Syntax
+var result = persons.Where(s => s.Contains('a'));
+
+Console.WriteLine("Query Syntax results:");
+foreach (var str in res)
+	Console.WriteLine(str);
+
+Console.WriteLine("Method Syntax results:");
+foreach (var str in result)
+    Console.WriteLine(str);
+
+Console.ReadLine();
